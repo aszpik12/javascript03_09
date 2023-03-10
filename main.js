6c6b6d79efc00000f266ba1c45adf4bdba92443e
PetrovaiAsztrik <asztrikpetrovai@gmail.com>
2023-03-10 11:00:54 +0100

    Képgalléria kisképrenyomva nagykép változik

--- main.js
+++ main.js
@@ -1,20 +1,49 @@
+let lista = [
+  "kepek/kep.jfif",
+  "kepek/kep.jpg",
+  "kepek/gods.png",
+  "kepek/icy.jpg",
+  "kepek/nurgle.jpg",
+  "kepek/boris.jpg",
+  "kepek/russia.jpg",
+  "kepek/smort.jpg",
+];
+
+let aktualisKep = 0;
+
 window.addEventListener("load", function () {
   const ARTICLEELEM = document.querySelectorAll("article");
-  let lista = [
-    "kepek/kep.jfif",
-    "kepek/kep.jpg",
-    "kepek/gods.png",
-    "kepek/icy.jpg",
-    "kepek/nurgle.jpg",
-    "kepek/boris.jpg",
-    "kepek/russia.jpg",
-    "kepek/smort.jpg",
-  ];
+  const SECTIONELEM = document.querySelectorAll("section");
+
   let szoveg = "<ul>";
   for (let i = 0; i < lista.length; i++) {
-    szoveg += `<li><img src='${lista[i]}' alt='' ></li>`;
+    szoveg += `<li><img src='${lista[i]}' alt='kep' onclick='changeImage(${i})'></li>`;
   }
   szoveg += "</ul>";
-  console.log(szoveg)
+  console.log(szoveg);
   ARTICLEELEM[0].innerHTML = szoveg;
+
+  document.getElementById("bal").addEventListener("click", prevImg);
+  document.getElementById("jobb").addEventListener("click", nextImg);
 });
+
+
+function changeImage(i) {
+  const image = document.querySelectorAll("section div img");
+  image[0].src = lista[i];
+  aktualisKep = i;
+}
+
+
+function nextImg() {
+  const image = document.querySelectorAll("section div img");
+  image[0].src = lista[aktualisKep + 1];
+  aktualisKep = aktualisKep + 1;
+}
+
+
+function prevImg() {
+  const image = document.querySelectorAll("section div img");
+  image[0].src = lista[aktualisKep - 1];
+  aktualisKep = aktualisKep - 1;
+}
--- script.js
+++ script.js
@@ -5,21 +5,24 @@ window.addEventListener("load", function () {
   ARTICLEELEM[0].innerHTML += "<button>OK</button>";
   const BUTTONELEM = document.querySelectorAll("article button");
 
-  BUTTONELEM[0].addEventListener("click", function(){
-    console.log(event.target)
+  
+  BUTTONELEM[0].addEventListener("click", function () {
+    console.log(event.target);
     const DIVELEM = document.querySelectorAll("article div");
-    console.log(DIVELEM[0])
+    console.log(DIVELEM[0]);
     DIVELEM[0].classList.add("formazas");
-  })
+  });
 
-  const KEPELEM=document.querySelectorAll("article div img")
-  KEPELEM[0].addEventListener("mouseover", function(event){
-    event.target.style.border="5px solid orange"
-    event.target.src="kep.jpg"
-  })
 
-  KEPELEM[0].addEventListener("mouseout", function(event){
-    event.target.style.border="0px solid orange"
-    event.target.src="kep.jfif"
-  })
+  const KEPELEM = document.querySelectorAll("article div img");
+  KEPELEM[0].addEventListener("mouseover", function (event) {
+    event.target.style.border = "5px solid orange";
+    event.target.src = "kep.jpg";
+  });
+
+
+  KEPELEM[0].addEventListener("mouseout", function (event) {
+    event.target.style.border = "0px solid orange";
+    event.target.src = "kep.jfif";
+  });
 });
